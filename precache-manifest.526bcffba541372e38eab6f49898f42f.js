780121d716354a99377e3098a934bbb2bb894fa1
Cheng-Kai Chen <cheng-kaichen@Cheng-Kais-MBP.hsd1.ca.comcast.net>
2020-05-19 11:36:08 -0700

    Updates

---
+++ precache-manifest.526bcffba541372e38eab6f49898f42f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee23db58dcc8a67252645f0ba116cedd",
+    "url": "/todo-demo/index.html"
+  },
+  {
+    "revision": "ddaac0e7edef2528b638",
+    "url": "/todo-demo/static/css/main.0dd8ff7d.chunk.css"
+  },
+  {
+    "revision": "efcdec7ad7be45248fc9",
+    "url": "/todo-demo/static/js/2.731cde75.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/todo-demo/static/js/2.731cde75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddaac0e7edef2528b638",
+    "url": "/todo-demo/static/js/main.47d0770e.chunk.js"
+  },
+  {
+    "revision": "e1e24af07fc7060137ce",
+    "url": "/todo-demo/static/js/runtime-main.ce67eb38.js"
+  }
+]);
